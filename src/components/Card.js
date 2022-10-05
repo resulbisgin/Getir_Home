59b9acec25147eb2c62dd5fe63c09667f56ec8be
resulbisgin <resulbiginresul_20011@hotmail.com>
2022-10-05 20:38:44 +0300

    create  Card.js

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,19 @@
-import React from 'react';
-
+import CardsData from "api/cards.json"
+import { useEffect, useState } from "react";
 function Card() {
+    const [cards,setCards]=useState([]);
+    useEffect(()=>{
+        setCards(CardsData)
+    },[])
     return (
-        <div>
-            Card
+        <div className="grid grid-cols-3 gap-x-4 ">
+             {cards.length && cards.map(card=>(
+                <div className="bg-white-10 rounded-lg p-14 shadow-lg flex flex-col items-center text-center">
+                    <img className="mb-6" src={card.image}/>
+                    <h6 className="font-semibold text-lg text-primary-brand-color">{card.title}</h6>
+                    <p className="mt-2 text-sm text-gray-700">{card.description}</p>
+                </div>
+             ))}
         </div>
     );
 }
