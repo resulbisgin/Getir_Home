629c95e0f30f1b5e0eb9b444a4d2cbad31492129
resulbisgin <resulbiginresul_20011@hotmail.com>
2022-10-05 15:34:46 +0300

    Create Categories

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,8 +1,23 @@
-
+import { useEffect, useState } from "react";
+import CategoryData from "../api/categories.json"
+import Category from "components/ui/Category";
 function Categories() {
+
+    const [categories,setCategories]=useState([]);
+    useEffect(()=>{
+        //İstek atıcam
+        setCategories(CategoryData)
+    },[])
     return (
-        <div>
-            Categories
+        <div className="bg-white py-4">
+           <div className="container mx-auto">
+            <h3 className="text-sm font-semibold mb-3">Kategoriler</h3>
+            <div className="grid grid-cols-10">
+                {categories && categories.map((category,index)=>
+                    <Category key={index} category={category}/>
+                )}
+            </div>
+           </div>
         </div>
     );
 }
